5eab869db7e8a351889185b074f41ab75ed1271b
cgmontea98 <cgmontealegre9804@gmail.com>
2023-02-09 21:57:05 -0500

    contador

--- src/components/EjerciciosD.jsx
+++ src/components/EjerciciosD.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React from "react";import Card from "react-bootstrap/Card";
 
 export const EjerciciosD = () => {
-  return<>
-  <h1>Ejercicios Dos</h1> <p>lorem</p>
-</>;
+  return (
+    <>
+      <h1>Ejercicios Dos</h1> <p>lorem</p>
+      <div className="my-4 d-flex align-items-center justify-content-center">
+        <ol><li></li><li></li><li></li><li></li><li></li></ol>
+      </div>
+    </>
+  );
 };
--- src/components/EjerciciosU.jsx
+++ src/components/EjerciciosU.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 export const EjerciciosU = () => {
+  //Para resetear
+  const [contador, setContador] = useState(0);
+  const [color, setColor] = useState("bg-secondary");
+  //Para que controle el cambio de color y es un bloque de código
+  useEffect(() => {
+    if (contador > 0) {
+      setColor("bg-success");
+    } else if (contador < 0) {
+      setColor("bg-danger");
+    } else {
+      setColor("bg-secondary");
+    }
+  }, [contador]);
+  //Clicks
+  const handleDec = () => {
+    const arrD = contador - 1;
+    setContador(arrD);
+  };
+  const handleInc = () => {
+    const arrI = contador + 1;
+    setContador(arrI);
+  };
+
   return (
     <>
-      <h1>Ejercicios Uno</h1> <p>lorem</p>
+      <h1>Ejercicios Uno</h1>
+      <h2>Contador</h2>
+      <p className={color}>{contador}</p>
+      <ButtonGroup aria-label="Basic example">
+        <Button variant="danger" onClick={() => handleDec()}>
+          Decrementar
+        </Button>
+        {/* Otra forma de usar el click es colocando la funcion del seteo directo en arrowfunction */}
+        <Button variant="secondary" onClick={() => setContador(0)}>
+          Resetear
+        </Button>
+        <Button variant="success" onClick={() => handleInc()}>
+          Incrementar
+        </Button>
+      </ButtonGroup>
     </>
   );
 };
