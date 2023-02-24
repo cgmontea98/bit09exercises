a91723059aa4a6b72b8a195074b9f6f613cda2eb
cgmontea98 <cgmontealegre9804@gmail.com>
2023-02-24 18:54:56 -0500

    modificando foto

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-
+import Me from "public\me.jpg"
 export const Inicio = () => {
   return (
     <>
       <div className="my-4 d-flex align-items-center justify-content-center">
         <Card style={{ width: "18rem" }} className="p-2">
-          <Card.Img variant="top" src="/me.jpg" className="rounded" alt="foto de perfil"/>
+          <Card.Img variant="top" src="{Me}" className="rounded" alt="foto de perfil"/>
           <Card.Body>
             <Card.Title>Cristian Montealegre</Card.Title>
           </Card.Body>
